084c1e37854b39dae387e3d9ffb5816f83935037
Sajid Sadeeq <sajidsadeeq@gmail.com>
2023-01-06 00:48:25 +0500

    second session

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,18 @@
+const H1 = React.createElement("h1", {
+    id: "title",
+    key: "title"
+}, "Hello Pakistan, Welcome to 2023");
+const H2 = React.createElement("h2", {
+    id: "title1",
+    key: "title2"
+}, "This is loveable");
+const container = React.createElement("div", {
+    id: "new-root"
+}, [
+    H1,
+    H2
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
